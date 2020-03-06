5d3128f859b7c727ff66048f576024c9832bd7d6
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-03-06 18:28:56 -0500

    Test only adding line if already reconciled

--- cap_account_reconciliation/static/src/js/reconciliation_renderer.js
+++ cap_account_reconciliation/static/src/js/reconciliation_renderer.js
@@ -31,69 +31,72 @@ odoo.define('cap_account_reconciliation.ReconciliationRenderer', function (requi
 
                 // GET THE DEFAULT TEMPLATE
                 var $line = $(qweb.render("reconciliation.line.mv_line", {'line': line, 'state': state}));
-                if (!isNaN(line.id)) {
-                    // ADD THREE COLUMNS: PARTNER NAME, INVOICE STORE ORDER ID, INVOICE CHANNEL NAME
-                    $line.find('.cell_label')
-                        .after('<td style="text-align: center" class="cell_channel"/>')
-                        .after('<td style="text-align: center" class="cell_store_order"/>')
-                        .after('<td style="text-align: center" class="cell_partner"/>');
-                    // SET DATA FOR THE PARTNER NAME COLUMN
-                    if (line.partner_name) {
-                        $line.find('.cell_partner').append(document.createTextNode(line.partner_name));
-                    }
+//                 if (!isNaN(line.id)) {
+//                     // ADD THREE COLUMNS: PARTNER NAME, INVOICE STORE ORDER ID, INVOICE CHANNEL NAME
+//                     $line.find('.cell_label')
+//                         .after('<td style="text-align: center" class="cell_channel"/>')
+//                         .after('<td style="text-align: center" class="cell_store_order"/>')
+//                         .after('<td style="text-align: center" class="cell_partner"/>');
+//                     // SET DATA FOR THE PARTNER NAME COLUMN
+//                     if (line.partner_name) {
+//                         $line.find('.cell_partner').append(document.createTextNode(line.partner_name));
+//                     }
                    
-                    // SET DATA FOR THE INVOICE STORE ORDER ID AND INVOICE CHANNEL NAME COLUMNS
-                    if (line.ref) {
-                        // TRY TO FIND AN INVOICE NUMBER REFERENCE FROM LINE LABEL OR NAME
-//                         var number = "";
-//                         if (line.label.startsWith('INV/')) {
-//                             number = line.label;
-//                         } else if (line.name.startsWith('INV/')) {
-//                             number = line.name;
-//                         } else if (line.label.includes('IN/')) {
-//                             number = line.label.match(/IN\/[^ :\/]*\/[^ :\/]*/g)[0];
-//                         } else if (line.name.includes('IN/')) {
-//                             number = line.name.match(/IN\/[^ :\/]*\/[^ :\/]*/g)[0];
-//                         }
-                        // IF WE FOUND ONE CORRECT INVOICE NUMBER
-//                         if (number.startsWith('INV/')) {
-                            // GET DATA FROM INVOICE
-//                             rpc.query({
+//                     // SET DATA FOR THE INVOICE STORE ORDER ID AND INVOICE CHANNEL NAME COLUMNS
+//                     if (line.ref) {
+//                         // TRY TO FIND AN INVOICE NUMBER REFERENCE FROM LINE LABEL OR NAME
+// //                         var number = "";
+// //                         if (line.label.startsWith('INV/')) {
+// //                             number = line.label;
+// //                         } else if (line.name.startsWith('INV/')) {
+// //                             number = line.name;
+// //                         } else if (line.label.includes('IN/')) {
+// //                             number = line.label.match(/IN\/[^ :\/]*\/[^ :\/]*/g)[0];
+// //                         } else if (line.name.includes('IN/')) {
+// //                             number = line.name.match(/IN\/[^ :\/]*\/[^ :\/]*/g)[0];
+// //                         }
+//                         // IF WE FOUND ONE CORRECT INVOICE NUMBER
+// //                         if (number.startsWith('INV/')) {
+//                             // GET DATA FROM INVOICE
+// //                             rpc.query({
+// //                                 model: 'account.move.line',
+// //                                 method: 'search_read',
+// //                                 args: [[['ref', '=', line.ref], ['invoice_id', '!=', false]], ['invoice_id']]
+// //                             }).then(function (result_move) {
+                                
+// //                                 alert(result_move[0]['invoice_id']);
+//                                 rpc.query({
 //                                 model: 'account.move.line',
 //                                 method: 'search_read',
-//                                 args: [[['ref', '=', line.ref], ['invoice_id', '!=', false]], ['invoice_id']]
-//                             }).then(function (result_move) {
-                                
-//                                 alert(result_move[0]['invoice_id']);
-                                rpc.query({
-                                model: 'account.move.line',
-                                method: 'search_read',
-                                args: [[['ref', '=', line.ref]], ['x_store_order_id', 'x_channel_name']]
-                                    }).then(function (result) {
+//                                 args: [[['ref', '=', line.ref]], ['x_store_order_id', 'x_channel_name']]
+//                                     }).then(function (result) {
 
                                     
-//                                     alert(result[0]['x_store_order_id']);
-                                    // SET DATA FOR THE INVOICE STORE ORDER ID COLUMN
-                                    if (result.length && result[0]['x_store_order_id']) {
-                                        $line.find('.cell_store_order')
-                                            .append(document.createTextNode(result[0]['x_store_order_id']));
-                                    }
-                                    // SET DATA FOR THE INVOICE CHANNEL ID COLUMN
-                                    if (result.length && result[0]['x_channel_name']) {
-                                        $line.find('.cell_channel')
-                                            .append(document.createTextNode(result[0]['x_channel_name']));
-                                    }
-                                });
-//                             });
-//                         }
-                    }
-                    // DO NOT FORGET TO SET UP THE BUBBLE INFO LIKE IN SUPER
-                    $('<span class="line_info_button fa fa-info-circle"/>')
-                        .appendTo($line.find('.cell_info_popover'))
-                        .attr("data-content", qweb.render('reconciliation.line.mv_line.details', {'line': line}));
-                }
+// //                                     alert(result[0]['x_store_order_id']);
+//                                     // SET DATA FOR THE INVOICE STORE ORDER ID COLUMN
+//                                     if (result.length && result[0]['x_store_order_id']) {
+//                                         $line.find('.cell_store_order')
+//                                             .append(document.createTextNode(result[0]['x_store_order_id']));
+//                                     }
+//                                     // SET DATA FOR THE INVOICE CHANNEL ID COLUMN
+//                                     if (result.length && result[0]['x_channel_name']) {
+//                                         $line.find('.cell_channel')
+//                                             .append(document.createTextNode(result[0]['x_channel_name']));
+//                                     }
+//                                 });
+// //                             });
+// //                         }
+//                     }
+//                     // DO NOT FORGET TO SET UP THE BUBBLE INFO LIKE IN SUPER
+//                     $('<span class="line_info_button fa fa-info-circle"/>')
+//                         .appendTo($line.find('.cell_info_popover'))
+//                         .attr("data-content", qweb.render('reconciliation.line.mv_line.details', {'line': line}));
+//                 }
                 // ADD RENDERED PROPOSITION TO THE MATCH TABLE
-                $mv_lines.append($line);
+                if (line.already_reconciled) {
+
+                    $mv_lines.append($line);
+                }
             });
         },
 
