8e42b5663d6c62df1a6c3d523e3edbd9c77b6f12
Sebastien Riss <39327688+sebastienriss@users.noreply.github.com>
2020-03-13 08:41:49 -0400

    Update account_reconciliation_widget.py

--- cap_account_reconciliation/models/account_reconciliation_widget.py
+++ cap_account_reconciliation/models/account_reconciliation_widget.py
@@ -21,7 +21,8 @@ class AccountReconciliationWidget(models.AbstractModel):
         
 #         for account_move_line_id in self.env['account.move.line'].search(domain):
 #             ids.append(account_move_line_id.id)
-        str_domain = ["|", ("x_channel_name", "ilike", search_str)] + str_domain
 
+        str_domain = ["account_id.id", "=", 263)] + str_domain
+#
         
         return str_domain
